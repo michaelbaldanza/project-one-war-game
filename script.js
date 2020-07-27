fdab921c08f52c3601255bb3072e176aa4e16d54
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-07-27 15:16:28 -0400

    created card rank arrays

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-let masterDeck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const jokers = ['joker-black', 'joker-red'];
+
+const aces = ['dA', 'hA', 'sA', 'cA'];
+
+const kings = ['dK', 'hK', 'sK', 'cK'];
+
+const queens = ['dQ', 'hQ', 'sQ', 'cQ'];
+
+const jacks = ['dJ', 'hJ', 'sJ', 'cJ'];
+
+const tens = ['d10', 'h10', 's10', 'c10'];
+
+const nines = ['d09', 'h09', 's09', 'c09'];
+
+const eights = ['d08', 'h08', 's08', 'c08'];
+
+const sevens = ['d07', 'h07', 's07', 'c07'];
+
+const sixes = ['d06', 'h06', 's06', 'c06'];
+
+const fives = ['d05', 'h05', 's05', 'c05'];
+
+const fours = ['d04', 'h04', 's04', 'c04'];
+
+const threes  = ['d03', 'h03', 's02', 'c02'];
+
+const twos = ['d02', 'h02', 's02', 'c02'];
+
+const masterDeck = [jokers, aces, kings, queens, jacks, tens, nines, eights, sevens, sixes, fives, fours, threes, twos];
 
 let playerDeck = [];
 
 let computerDeck = [];
+
+function deal() [
+    
+]
+
 
