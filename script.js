c28c281d0c270de093ce95688449fab6bf0aaa49
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-07-28 16:11:22 -0400

    remodel data deck arrays instead of classes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//
+
 class Card {
     constructor(cssname, cardname, value) {
         this.cardname = cardname;
@@ -6,82 +8,194 @@ class Card {
     };
 };
 
-class Deck {
-    constructor(contents) {
-        this.contents = [];
-    };
-    getTopCard() {
-        return contents[0];
-    };
-    addCard(card) {
-        this.contents.push(card);
-    };
-    splitRandomly() {};
+//
+let mDeck = [
+    new Card('Ace of Diamonds', 'dA', 14),
+    new Card('King of Diamonds', 'dK', 13),
+    new Card('Queen of Diamonds', 'dQ', 12),
+    new Card('Jack of Diamonds', 'dJ', 11),
+    new Card('Ten of Diamonds', 'd10', 10),
+    new Card('Nine of Diamonds', 'd09', 9),
+    new Card('Eight of Diamonds', 'd08', 8),
+    new Card('Seven of Diamonds', 'd07', 7),
+    new Card('Six of Diamonds', 'd06', 6),
+    new Card('Five of Diamonds', 'd05', 5),
+    new Card('Four of Diamonds', 'd04', 4),
+    new Card('Three of Diamonds', 'd03', 3),
+    new Card('Two of Diamonds', 'd02', 2),
+    new Card('Ace of Hearts', 'hA', 14),
+    new Card('King of Hearts', 'hK', 13),
+    new Card('Queen of Hearts', 'hQ', 12),
+    new Card('Jack of Hearts', 'hJ', 11),
+    new Card('Ten of Hearts', 'h10', 10),
+    new Card('Nine of Hearts', 'h09', 9),
+    new Card('Eight of Hearts', 'h08', 8),
+    new Card('Seven of Hearts', 'h07', 7),
+    new Card('Six of Hearts', 'h06', 6),
+    new Card('Five of Hearts', 'h05', 5),
+    new Card('Four of Hearts', 'h04', 4),
+    new Card('Three of Hearts', 'h03', 3),
+    new Card('Two of Hearts', 'h02', 2),
+    new Card('Ace of Spades', 'sA', 14),
+    new Card('King of Spades', 'sK', 13),
+    new Card('Queen of Spades', 'sQ', 12),
+    new Card('Jack of Spades', 'sJ', 11),
+    new Card('Ten of Spades', 's10', 10),
+    new Card('Nine of Spades', 's09', 9),
+    new Card('Eight of Spades', 's08', 8),
+    new Card('Seven of Spades', 's07', 7),
+    new Card('Six of Spades', 's06', 6),
+    new Card('Five of Spades', 's05', 5),
+    new Card('Four of Spades', 's04', 4),
+    new Card('Three of Spades', 's03', 3),
+    new Card('Two of Spades', 's02', 2),
+    new Card('Ace of Clubs', 'cA', 14),
+    new Card('King of Clubs', 'cK', 13),
+    new Card('Queen of Clubs', 'cQ', 12),
+    new Card('Jack of Clubs', 'cJ', 11),
+    new Card('Ten of Clubs', 'c10', 10),
+    new Card('Nine of Clubs', 'c09', 9),
+    new Card('Eight of Clubs', 'c08', 8),
+    new Card('Seven of Clubs', 'c07', 7),
+    new Card('Six of Clubs', 'c06', 6),
+    new Card('Five of Clubs', 'c05', 5),
+    new Card('Four of Clubs', 'c04', 4),
+    new Card('Three of Clubs', 'c03', 3),
+    new Card('Two of Clubs', 'c02', 2),
+    new Card('Red Joker', 'rJ', 15),
+    new Card('Black Joker', 'bJ', 15),
+];
 
-};
+let p1Deck = mDeck.splice(27, 27);
+let p2Deck = mDeck.splice(55, 27);
+
+console.log(p1Deck);
+
+// masterDeck[30].cardname;
+// console.log(masterDeck.filter(x => x.cardname === 'Ace of Diamonds').length);
+// forEach(console.log(x.cardname));
+//check randomizer by filtering deck - iterate ind values ot get names, 
+//then filter values to check - length greater than one, randomizer mech is broken
+
+// class Deck {
+//     constructor(contents) {
+//         this.contents = [];
+//     };
+//     playTopCard() {
+//         return contents[0];
+//     };
+//     addCard(card) {
+//         this.contents.push(Card);
+//     };
+//     splitRandomly() {
+
+//     };
+//     shuffleDeck() {
+
+//     };
+
+// };
 
 
-const masterDeck = new Deck();
-masterDeck.addCard(n01);
-masterDeck.addCard(n02);
 
 
-const n01 = new Card('Ace of Diamonds', 'dA', 14);
-const n02 = new Card('King of Diamonds', 'dK', 13);
-const n03 = new Card('Queen of Diamonds', 'dQ', 12);
-const n04 = new Card('Jack of Diamonds', 'dJ', 11);
-const n05 = new Card('Ten of Diamonds', 'd10', 10);
-const n06 = new Card('Nine of Diamonds', 'd09', 9);
-const n07 = new Card('Eight of Diamonds', 'd08', 8);
-const n08 = new Card('Seven of Diamonds', 'd07', 7);
-const n09 = new Card('Six of Diamonds', 'd06', 6);
-const n10 = new Card('Five of Diamonds', 'd05', 5);
-const n11 = new Card('Four of Diamonds', 'd04', 4);
-const n12 = new Card('Three of Diamonds', 'd03', 3);
-const n13 = new Card('Two of Diamonds', 'd02', 2);
-const n14 = new Card('Ace of Hearts', 'hA', 14);
-const n15 = new Card('King of Hearts', 'hK', 13);
-const n16 = new Card('Queen of Hearts', 'hQ', 12);
-const n17 = new Card('Jack of Hearts', 'hJ', 11);
-const n18 = new Card('Ten of Hearts', 'h10', 10);
-const n19 = new Card('Nine of Hearts', 'h09', 9);
-const n20 = new Card('Eight of Hearts', 'h08', 8);
-const n21 = new Card('Seven of Hearts', 'h07', 7);
-const n22 = new Card('Six of Hearts', 'h06', 6);
-const n23 = new Card('Five of Hearts', 'h05', 5);
-const n24 = new Card('Four of Hearts', 'h04', 4);
-const n25 = new Card('Three of Hearts', 'h03', 3);
-const n26 = new Card('Two of Hearts', 'h02', 2);
-const n27 = new Card('Ace of Spades', 'sA', 14);
-const n28 = new Card('King of Spades', 'sK', 13);
-const n29 = new Card('Queen of Spades', 'sQ', 12);
-const n30 = new Card('Jack of Spades', 'sJ', 11);
-const n31 = new Card('Ten of Spades', 's10', 10);
-const n32 = new Card('Nine of Spades', 's09', 9);
-const n33 = new Card('Eight of Spades', 's08', 8);
-const n34 = new Card('Seven of Spades', 's07', 7);
-const n35 = new Card('Six of Spades', 's06', 6);
-const n36 = new Card('Five of Spades', 's05', 5);
-const n37 = new Card('Four of Spades', 's04', 4);
-const n38 = new Card('Three of Spades', 's03', 3);
-const n39 = new Card('Two of Spades', 's02', 2);
-const n40 = new Card('Ace of Clubs', 'cA', 14);
-const n41 = new Card('King of Clubs', 'cK', 13);
-const n42 = new Card('Queen of Clubs', 'cQ', 12);
-const n43 = new Card('Jack of Clubs', 'cJ', 11);
-const n44 = new Card('Ten of Clubs', 'c10', 10);
-const n45 = new Card('Nine of Clubs', 'c09', 9);
-const n46 = new Card('Eight of Clubs', 'c08', 8);
-const n47 = new Card('Seven of Clubs', 'c07', 7);
-const n48 = new Card('Six of Clubs', 'c06', 6);
-const n49 = new Card('Five of Clubs', 'c05', 5);
-const n50 = new Card('Four of Clubs', 'c04', 4);
-const n51 = new Card('Three of Clubs', 'c03', 3);
-const n52 = new Card('Two of Clubs', 'c02', 2);
-const n53 = new Card('Red Joker', 'rJ', 15);
-const n54 = new Card('Black Joker', 'bJ', 15);
+// fucntion ()
 
-const masterDeck = [n01, n02, n03, n04, n05, n06, n07, n08, n09, n10, n11, n12, n13, n14, n15, n16, n17, n18, n19, n20, n21, n22, n23, n24, n25, n26, n27, n28, n29, n30, n31, n32, n33, n3]
+// Decks
+
+// const mDeck = [n01, n02, n03, n04, n05, n06, n07, n08, n09, n10, n11, n12, n13, n14, n15, n16, n17, n18, n19, n20, n21, n22, n23, n24, n25, n26, n27, n28, n29, n30, n31, n32, n33, n34, n35, n36, n37, n38, n39, n40, n41, n42, n43, n44, n45, n46, n47, n48, n49, n50, n51, n52, n53, n54];
+
+// mDeck.filter((element, index, mDeck)
+//     for (i=0; i<mDeck.length; i++) {
+        
+//     }
+
+// );
+
+ 
+
+
+
+
+// let p1Discard = [];
+
+// let p2Discard = [];
+
+
+
+// const mDeck = new Deck();
+
+// let p1Deck = new Deck();
+
+// let p2Deck = new Deck();
+
+// let p1Discard = new Deck();
+
+// let p2Discard = new Deck();
+
+
+
+
+// mDeck.addCard(n01);
+// mDeck.addCard(n02);
+// mDeck.addCard(n03);
+// mDeck.addCard(n04);
+// mDeck.addCard(n05);
+// mDeck.addCard(n06);
+// mDeck.addCard(n07);
+// mDeck.addCard(n08);
+// mDeck.addCard(n09);
+// mDeck.addCard(n10);
+// mDeck.addCard(n11);
+// mDeck.addCard(n12);
+// mDeck.addCard(n13);
+// mDeck.addCard(n14);
+// mDeck.addCard(n15);
+// mDeck.addCard(n16);
+// mDeck.addCard(n17);
+// mDeck.addCard(n18);
+// mDeck.addCard(n19);
+// mDeck.addCard(n20);
+// mDeck.addCard(n21);
+// mDeck.addCard(n22);
+// mDeck.addCard(n23);
+// mDeck.addCard(n24);
+// mDeck.addCard(n25);
+// mDeck.addCard(n26);
+// mDeck.addCard(n27);
+// mDeck.addCard(n28);
+// mDeck.addCard(n29);
+// mDeck.addCard(n30);
+// mDeck.addCard(n31);
+// mDeck.addCard(n32);
+// mDeck.addCard(n33);
+// mDeck.addCard(n34);
+// mDeck.addCard(n35);
+// mDeck.addCard(n36);
+// mDeck.addCard(n37);
+// mDeck.addCard(n38);
+// mDeck.addCard(n39);
+// mDeck.addCard(n40);
+// mDeck.addCard(n41);
+// mDeck.addCard(n42);
+// mDeck.addCard(n43);
+// mDeck.addCard(n44);
+// mDeck.addCard(n45);
+// mDeck.addCard(n46);
+// mDeck.addCard(n47);
+// mDeck.addCard(n48);
+// mDeck.addCard(n49);
+// mDeck.addCard(n50);
+// mDeck.addCard(n51);
+// mDeck.addCard(n52);
+// mDeck.addCard(n53);
+// mDeck.addCard(n54);
+// mDeck.splitRandomly();
+
+
+
+
+
 
 // const jacks = ['dJ', 'hJ', 'sJ', 'cJ'];
 
@@ -105,12 +219,12 @@ const masterDeck = [n01, n02, n03, n04, n05, n06, n07, n08, n09, n10, n11, n12, 
 
 // const masterDeck = [jokers, aces, kings, queens, jacks, tens, nines, eights, sevens, sixes, fives, fours, threes, twos];
 
-let playerDeck = [];
+// let playerDeck = [];
 
-let computerDeck = [];
+// let computerDeck = [];
 
-function deal() [
+// function deal() [
     
-]
+// ]
 
 
